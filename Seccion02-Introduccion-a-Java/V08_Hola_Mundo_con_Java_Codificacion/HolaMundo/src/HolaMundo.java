ac27f199cd171370818adcd16e28ee1866fdb72d
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-08-17 01:19:56 +0200

    Hola Mundo Codigo

---
+++ Seccion02-Introduccion-a-Java/V08_Hola_Mundo_con_Java_Codificacion/HolaMundo/src/HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo {
+    // Agregamos el metodo principal
+    public static void main(String[] args) {
+        System.out.println("Hola desde Java");
+        System.out.println("Saludos");
+    }
+}
