98a916f56ce839f25cf02f89c778183bae05372c
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-08-17 11:59:18 +0200

    Presentate con Java

--- Seccion02-Introduccion-a-Java/V09_Reto_Presentate_con_Java/README.md
+++ Seccion02-Introduccion-a-Java/V09_Reto_Presentate_con_Java/README.md
@@ -1,3 +1,5 @@
 # Reto - Presentate con Java
 
 ![Ejercicio](Docs/Presentate-con-Java.jpg)
+
+* [Clase Presentae](Resolucion_Reto_presentate_Con_Java/src/Presentate.java)
---
+++ Seccion02-Introduccion-a-Java/V10_Solucion_Presentate_con_Java/Docs/01-09-00-Solucion-Presentate-UJ.java
@@ -0,0 +1,7 @@
+public class Presentate {
+    public static void main(String[] args) {
+        System.out.println("Nombre: Juan Perez");
+        System.out.println("Edad: 28 años");
+        System.out.println("Pais: México");
+    }
+}
