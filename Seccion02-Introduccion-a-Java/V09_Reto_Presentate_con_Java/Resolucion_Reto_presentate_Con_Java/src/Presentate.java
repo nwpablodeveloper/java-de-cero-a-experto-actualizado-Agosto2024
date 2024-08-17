59bc55251f492ba21d121c5018929d7e3bc0a504
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-08-17 11:55:42 +0200

    V09 Presentate con Java

---
+++ Seccion02-Introduccion-a-Java/V09_Reto_Presentate_con_Java/Resolucion_Reto_presentate_Con_Java/src/Presentate.java
@@ -0,0 +1,7 @@
+public class Presentate {
+    public static void main(String[] args) {
+        System.out.println("Pablo Veiga");
+        System.out.println(36);
+        System.out.println("Argentina/Italia");
+    }
+}
