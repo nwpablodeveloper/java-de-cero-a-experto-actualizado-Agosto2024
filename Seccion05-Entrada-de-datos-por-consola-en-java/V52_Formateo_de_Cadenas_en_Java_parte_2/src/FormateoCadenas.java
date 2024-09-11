8570a28bfbc7cd3c5eff539d3bc1e1e27e185c17
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-09-11 13:32:28 +0200

    Rellenar con 0 y dar mas formatos a un text Block

--- Seccion05-Entrada-de-datos-por-consola-en-java/V52_Formateo_de_Cadenas_en_Java_parte_2/src/FormateoCadenas.java
+++ Seccion05-Entrada-de-datos-por-consola-en-java/V52_Formateo_de_Cadenas_en_Java_parte_2/src/FormateoCadenas.java
@@ -1,13 +1,32 @@
 public class FormateoCadenas {
     public static void main(String[] args) {
-        System.out.println("\n *** Formateo de Cadenas parte 2 ***");
+        System.out.printf("%n *** Formateo de Cadenas parte 2 ***");
 
         //Formateo con Text Block
+        var nombre = "Matías";
+        var edad = 35;
+        var salario = 250000.52;
+        var numeroEmpleado = 192;
 
         var mensaje = """
-                %nDetalle de Persona:
-                """;
+                %nDetalle de Persona:\s
+                ----------------------
+                \tNombre: %s
+                \tNúmero de empleado: %08d
+                \tEdad: %d años
+                \tSalario: $ %.2f
+                """.formatted(nombre, numeroEmpleado,edad, salario);
 
-        System.out.println("mensaje = " + mensaje);
+        System.out.printf(mensaje);
+
+        // Formateo de text Block y printf directamente
+        System.out.printf("""
+                %nDetalle de Persona:\s
+                ----------------------
+                \tNombre: %s
+                \tNúmero de empleado: %08d
+                \tEdad: %d años
+                \tSalario: $ %.2f
+                """, nombre, numeroEmpleado,edad,salario);
     }
 }
