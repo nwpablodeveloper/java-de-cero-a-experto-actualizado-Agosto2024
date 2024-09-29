dad5c0ab59a81e085c6c27aaf1fe4c64627439be
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-09-30 00:14:37 +0200

    Ciclo While

--- Seccion08-Ciclos-en-java/V104_Ejercicio_Ciclo_While_en_Java/src/CicloWhile.java
+++ Seccion08-Ciclos-en-java/V104_Ejercicio_Ciclo_While_en_Java/src/CicloWhile.java
@@ -2,6 +2,10 @@ public class CicloWhile {
     public static void main(String[] args) {
         System.out.println("\n*** Ciclo While ***");
 
-        var contador
+        var contador = 1;
+
+        while ( contador <= 5 )
+            System.out.println(contador++);
+
     }
 }
