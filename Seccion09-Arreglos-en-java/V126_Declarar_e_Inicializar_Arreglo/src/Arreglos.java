bb393eb0376d5bed375a367139c05bc04a6e12c0
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-10-10 17:49:45 +0200

    Inicializar Arreglos

---
+++ Seccion09-Arreglos-en-java/V126_Declarar_e_Inicializar_Arreglo/src/Arreglos.java
@@ -0,0 +1,16 @@
+public class Arreglos {
+    public static void main(String[] args) {
+        System.out.println("*** Arreglos en Java ***");
+
+        // 2. Declarar un Arreglo
+        int[] enteros;
+        // 3. Inicializar el arreglo para reservar el espacio en memoria
+        enteros = new int[3];
+
+        // Declarar e inicializar un arreglo en 1 sola linea
+        int[] numeros = new int[2];
+        var numeros2 = new int[2];
+
+
+    }
+}
