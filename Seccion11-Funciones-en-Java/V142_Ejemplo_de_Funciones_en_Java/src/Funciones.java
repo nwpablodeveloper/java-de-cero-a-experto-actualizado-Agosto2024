84f45823d00f743fb79cf28d135426ac45efd858
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-10-13 23:37:11 +0200

    Ejemplo de una función

--- Seccion11-Funciones-en-Java/V142_Ejemplo_de_Funciones_en_Java/src/Funciones.java
+++ Seccion11-Funciones-en-Java/V142_Ejemplo_de_Funciones_en_Java/src/Funciones.java
@@ -1,11 +1,16 @@
 public class Funciones {
 
+    /*
+        Función declarada, los contenidos recibidos son llamados como
+        parametros
+     */
     static void saludar(String mensaje){
         System.out.println("Mensaje: " + mensaje);
     }
 
     public static void main(String[] args) {
         // Funciones en Java
+        // Los contendios enviados los llamamos Argumentos
         saludar("Hola desde Java");
     }
 }
