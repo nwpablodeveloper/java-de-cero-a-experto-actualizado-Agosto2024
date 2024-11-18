31189a65732b5461eefa9f75feb56d58c9d74c41
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-11-18 18:41:43 +0100

    V261 Crear Objeto Login Form con Swing

--- Seccion17-Aplicaciones-de-Escritorio-GUI-con-Swing-y-Java/V261_Crear_Objeto_Login_Form_con_Swing/src/main/java/LoginForm.java
+++ Seccion17-Aplicaciones-de-Escritorio-GUI-con-Swing-y-Java/V261_Crear_Objeto_Login_Form_con_Swing/src/main/java/LoginForm.java
@@ -1,12 +1,27 @@
+import com.formdev.flatlaf.FlatDarculaLaf;
+
 import javax.swing.*;
 
-public class LoginForm {
+public class LoginForm extends JFrame{
     private JPanel PanelPrincipal;
     private JTextField usuarioTexto;
     private JPasswordField passwordTexto;
     private JButton enviarBoton;
 
-    public static void main(String[] args) {
+    public LoginForm(){
+        inicializarForma();
+    }
 
+    private void inicializarForma(){
+        setContentPane(PanelPrincipal);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(250,400);
+        setLocationRelativeTo(null); // Centrar la ventana
+    }
+
+    public static void main(String[] args) {
+        FlatDarculaLaf.setup();
+        LoginForm loginForm = new LoginForm();
+        loginForm.setVisible(true);
     }
 }
