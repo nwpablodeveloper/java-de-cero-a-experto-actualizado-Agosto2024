b6eb723f387e8f9b7245e59848e78cf38cba5894
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-11-18 18:31:32 +0100

    V260 Creacion del Componente da Vista de Login con Swing

---
+++ Seccion17-Aplicaciones-de-Escritorio-GUI-con-Swing-y-Java/V260_Creacion_del_Componente_de_Vista_del_Login_con_Swing/src/main/java/LoginForm.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class LoginForm {
+    private JPanel PanelPrincipal;
+    private JTextField usuarioTexto;
+    private JPasswordField passwordTexto;
+    private JButton enviarBoton;
+
+    public static void main(String[] args) {
+
+    }
+}
