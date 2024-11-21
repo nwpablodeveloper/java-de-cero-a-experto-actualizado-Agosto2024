0596bda9248950f13557c87abccea43e334f81a6
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-11-21 16:51:08 +0100

    Creación de la Clase Forma

--- Seccion17-Aplicaciones-de-Escritorio-GUI-con-Swing-y-Java/V264_Creacion_de_la_Clase_Forma_con_Swing/src/main/java/gm/zona_fit/gui/ZonaFitForma.java
+++ Seccion17-Aplicaciones-de-Escritorio-GUI-con-Swing-y-Java/V264_Creacion_de_la_Clase_Forma_con_Swing/src/main/java/gm/zona_fit/gui/ZonaFitForma.java
@@ -1,7 +1,23 @@
 package gm.zona_fit.gui;
 
+import gm.zona_fit.servicio.ClienteServicio;
+import gm.zona_fit.servicio.IClienteServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import javax.swing.*;
 
-public class ZonaFitForma {
+
+@Component
+public class ZonaFitForma extends JFrame{
+
     private JPanel panelPrincipal;
+    IClienteServicio clienteServicio;
+
+    // Inyeccion de dependencias
+    @Autowired
+    public ZonaFitForma(ClienteServicio clienteServicio){
+        this.clienteServicio = clienteServicio;
+    }
+
 }
