fa644df6e3672cac4705510283b28302e1e1342a
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-11-23 23:25:36 +0100

    V268 Creacion de los Cabeceros de la Tabla Swing

--- Seccion17-Aplicaciones-de-Escritorio-GUI-con-Swing-y-Java/README.md
+++ Seccion17-Aplicaciones-de-Escritorio-GUI-con-Swing-y-Java/README.md
@@ -41,6 +41,7 @@ Sección 17: Aplicaciones de Escritorio (GUI) con Swing y Java
 * [V263 Demo App Zona Fit con Swing y Spring](V263_Demo_App_Zona_Fit_con_Swing_y_Spring/demo-zona-fit.jpg)
 * [V264 Creacion de la Clase Forma con Swing]()
 * [V265 Ejecucion Inicial App Zona Fit con Swing y Spring](V265_Ejecucion_Inicial_App_Zona_Fit_con_Swing_y_Spring)
+* [V266 Activar modo oscuro](V267_Creacion_de_la_Tabla_de_Listado_de_Clientes_con_Swing/src/main/java/gm/zona_fit/ZonaFitSwing.java)
 * [V267 Creacion de la Tabla de Listado de Clientes con Swing](V267_Creacion_de_la_Tabla_de_Listado_de_Clientes_con_Swing)
     * Activar el Scroll para la tabla
         - JTable
--- Seccion17-Aplicaciones-de-Escritorio-GUI-con-Swing-y-Java/V268_Creacion_de_los_Cabeceros_de_la_Tabla_Swing/src/main/java/gm/zona_fit/gui/ZonaFitForma.java
+++ Seccion17-Aplicaciones-de-Escritorio-GUI-con-Swing-y-Java/V268_Creacion_de_los_Cabeceros_de_la_Tabla_Swing/src/main/java/gm/zona_fit/gui/ZonaFitForma.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 
 @Component
@@ -15,6 +16,7 @@ public class ZonaFitForma extends JFrame{
     private JPanel panelPrincipal;
     private JTable clientesTabla;
     IClienteServicio clienteServicio;
+    private DefaultTableModel tablaModeloClientes;
 
     // Inyeccion de dependencias
     @Autowired
@@ -31,4 +33,11 @@ public class ZonaFitForma extends JFrame{
         setLocationRelativeTo(null);
     }
 
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        this.tablaModeloClientes = new DefaultTableModel(0, 4);
+        String[] cabeceros = {"Id","Nombre","Apellido","Membresia"};
+        this.tablaModeloClientes.setColumnIdentifiers(cabeceros);
+        this.clientesTabla = new JTable(tablaModeloClientes);
+    }
 }
