c5c7b44cb92db4a9231b2662b8657514629a089c
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-11-24 10:21:08 +0100

    V269 Listado de Clientes con Swing

--- Seccion17-Aplicaciones-de-Escritorio-GUI-con-Swing-y-Java/V269_Listado_de_Clientes_con_Swing/src/main/java/gm/zona_fit/gui/ZonaFitForma.java
+++ Seccion17-Aplicaciones-de-Escritorio-GUI-con-Swing-y-Java/V269_Listado_de_Clientes_con_Swing/src/main/java/gm/zona_fit/gui/ZonaFitForma.java
@@ -1,6 +1,7 @@
 package gm.zona_fit.gui;
 
 import com.formdev.flatlaf.FlatDarculaLaf;
+import gm.zona_fit.modelo.Cliente;
 import gm.zona_fit.servicio.ClienteServicio;
 import gm.zona_fit.servicio.IClienteServicio;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.util.List;
 
 
 @Component
@@ -21,7 +23,6 @@ public class ZonaFitForma extends JFrame{
     // Inyeccion de dependencias
     @Autowired
     public ZonaFitForma(ClienteServicio clienteServicio){
-        FlatDarculaLaf.setup();
         this.clienteServicio = clienteServicio;
         iniciarForma();
     }
@@ -39,5 +40,22 @@ public class ZonaFitForma extends JFrame{
         String[] cabeceros = {"Id","Nombre","Apellido","Membresia"};
         this.tablaModeloClientes.setColumnIdentifiers(cabeceros);
         this.clientesTabla = new JTable(tablaModeloClientes);
+
+        // Cargar listado de clientes
+        listarClientes();
+    }
+
+    private void listarClientes(){
+        this.tablaModeloClientes.setRowCount(0);
+        List<Cliente> clientes = this.clienteServicio.listarClientes();
+        clientes.forEach(cliente -> {
+            Object[] renglonCliente = {
+                    cliente.getId(),
+                    cliente.getNombre(),
+                    cliente.getApellido(),
+                    cliente.getMembresia()
+            };
+            this.tablaModeloClientes.addRow(renglonCliente);
+        });
     }
 }
